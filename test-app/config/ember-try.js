43438eb34a97579fbc5c67841a2669252b1aa4f8
Shirin <shirin.boomi.dev@gmail.com>
2023-08-20 12:35:09 +0200

    Add embroider scenarios

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
@@ -50,6 +51,46 @@ module.exports = async function () {
           },
         },
       },
+      embroiderSafe({
+        name: 'ember-lts-4.12 + embroider-safe',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      }),
+      embroiderSafe({
+        name: 'ember-lts-5.0 + embroider-safe',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.0.0',
+          },
+        },
+      }),
+      embroiderOptimized({
+        name: 'ember-lts-4.12 + embroider-optimized',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+          },
+        },
+      }),
+      embroiderOptimized({
+        name: 'ember-lts-5.0 + embroider-optimized',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.0.0',
+          },
+        },
+      }),
+      embroiderOptimized({
+        name: 'ember-release + embroider-optimized',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release'),
+          },
+        },
+      }),
     ],
   };
 };
