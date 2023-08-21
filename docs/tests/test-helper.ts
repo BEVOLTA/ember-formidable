300d5b606c575d873bdb029baa761551063cd124
Shirin <shirin.boomi.dev@gmail.com>
2023-08-21 15:08:28 +0200

    Add deploy packages for docs

--- docs/tests/test-helper.ts
+++ docs/tests/test-helper.ts
@@ -1,9 +1,10 @@
-import Application from 'docs/app';
-import config from 'docs/config/environment';
-import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
+
+import Application from 'docs/app';
+import config from 'docs/config/environment';
 
 setApplication(Application.create(config.APP));
 
