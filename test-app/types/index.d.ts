19ba4782f343baa7316c349b6df25f97003eeb3c
Shirin <shirin.boomi.dev@gmail.com>
2023-08-29 14:15:02 +0200

    Add registry

--- test-app/types/index.d.ts
+++ test-app/types/index.d.ts
@@ -1,16 +1,14 @@
+// Types for compiled templates
 import 'ember-source/types';
 import 'ember-source/types/preview';
 import '@glint/environment-ember-loose';
 
 import type { HelperLike } from '@glint/template';
-import type { Formidable } from 'ember-formidable';
+import type FormidableRegistry from 'ember-formidable/template-registry';
 declare module '@glint/environment-ember-loose/registry' {
-  // Remove this once entries have been added! 👇
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export default interface Registry {
+  export default interface Registry extends FormidableRegistry {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
     'page-title': HelperLike<{ Args: { Positional: [string] }; Return: string }>;
-    Formidable: Formidable;
   }
 }
